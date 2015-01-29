75afc277ce73172abb1345762814ea12d1952061
jimjshields <jim.j.shields@gmail.com>
2015-01-29 15:41:17 -0500

    Added simple routing functionality and functions.

--- framework.py
+++ framework.py
@@ -5,8 +5,40 @@ class application:
 		self.environ = environ
 		self.start_response = start_response
 
+	# def __iter__(self):
+	# 	status = '200 OK'
+	# 	response_headers = [('Content-type', 'text/plain')]
+	# 	self.start(status, response_headers)
+	# 	yield 'Hello world!'
+
 	def __iter__(self):
+		path = self.environ['PATH_INFO']
+		if path == '/':
+			return self.GET_index()
+		elif path == '/hello':
+			return self.GET_hello()
+		else:
+			return self.notfound()
+
+	def GET_index(self):
 		status = '200 OK'
-		response_headers = [('Content-type', 'text/plain')]
+		response_headers = [('Content-type', 'text.plain')]
+		self.start(status, response_headers)
+		yield 'Welcome!'
+
+	def GET_hello(self):
+		status = '200 OK'
+		response_headers = [('Content-type', 'text.plain')]
 		self.start(status, response_headers)
 		yield 'Hello world!'
+
+	def notfound(self):
+		status = '404 Not Found'
+		response_headers = [('Content-type', 'text.plain')]
+		self.start(status, response_headers)
+		yield 'Not Found.'
+
+if __name__ == '__main__':
+	from wsgiref.simple_server import make_server
+	httpd = make_server('localhost', 8080, application)
+	httpd.handle_request()
