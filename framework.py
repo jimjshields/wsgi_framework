d84ed68c05845dab5791462ec57fd5e37ef22a2d
jimjshields <jim.j.shields@gmail.com>
2015-01-29 15:48:07 -0500

    Added pattern matching for URLs.

--- framework.py
+++ framework.py
@@ -1,5 +1,7 @@
 # From http://anandology.com/blog/how-to-write-a-web-framework-in-python/
 
+import re
+
 class application:
 	def __init__(self, environ, start_response):
 		self.environ = environ
@@ -12,29 +14,42 @@ class application:
 	# 	yield 'Hello world!'
 
 	def __iter__(self):
-		path = self.environ['PATH_INFO']
-		if path == '/':
-			return self.GET_index()
-		elif path == '/hello':
-			return self.GET_hello()
-		else:
-			return self.notfound()
+		return self.delegate()
+
+		def delegate(self):
+			path = self.environ['PATH_INFO']
+			method = self.environ['REQUEST_METHOD']
+
+			for pattern, name in self.urls:
+				m = re.match('^' + pattern + '$', path)
+				if m:
+					args = m.groups()
+					func_name = method.upper() + "_" + name
+					func = getattr(self, func_name)
+					return func(*args)
+
+			return self.not_found()
+
+	urls = [
+			("/", "index"),
+			("/hello/(.*)", "hello")
+	]
 
 	def GET_index(self):
 		status = '200 OK'
-		response_headers = [('Content-type', 'text.plain')]
+		response_headers = [('Content-type', 'text/plain')]
 		self.start(status, response_headers)
 		yield 'Welcome!'
 
-	def GET_hello(self):
+	def GET_hello(self, name):
 		status = '200 OK'
-		response_headers = [('Content-type', 'text.plain')]
+		response_headers = [('Content-type', 'text/plain')]
 		self.start(status, response_headers)
-		yield 'Hello world!'
+		yield 'Hello %n!' % (name)
 
-	def notfound(self):
+	def not_found(self):
 		status = '404 Not Found'
-		response_headers = [('Content-type', 'text.plain')]
+		response_headers = [('Content-type', 'text/plain')]
 		self.start(status, response_headers)
 		yield 'Not Found.'
 
